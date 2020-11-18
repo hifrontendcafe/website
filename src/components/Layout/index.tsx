66e55570227ede0b384b90829f1f86ee9ceda004
Martin Bavio <mbavio@gmail.com>
2020-11-18 15:11:13 -0300

    Complete simple landing

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,54 +1,55 @@
-import Head from 'next/head';
 import Header from '../Header';
 import Footer from '../Footer';
 import PreviewBanner from '../PreviewBanner';
+import FecHead from '../FecHead';
+import { ReactElement } from 'react';
 
 interface LayoutProps {
-  title: string;
+  mode?: string;
+  title?: string;
   description?: string;
   preview?: boolean;
 }
 
+const MainLayout: React.FC = ({
+  preview,
+  children,
+}: {
+  preview: boolean;
+  children: ReactElement;
+}) => (
+  <div className="antialiased">
+    {preview && <PreviewBanner />}
+    <Header preview={preview} />
+    {children}
+    <Footer />
+  </div>
+);
+
+const SimpleLayout: React.FC = ({
+  preview,
+  children,
+}: {
+  preview: boolean;
+  children: ReactElement;
+}) => <div className="antialiased">{children}</div>;
+
 const Layout: React.FC<LayoutProps> = ({
+  mode = 'main',
+  title,
+  description,
   preview = false,
   children,
-  title = 'FrontEndCafé',
-  description = 'Somos una comunidad de personas interesadas en tecnología y ciencias informáticas en donde charlamos sobre lenguajes de programación, diseño web, infraestructura, compartimos dudas, preguntamos y respondemos.',
 }) => {
   return (
     <>
-      <Head>
-        <title>{title} - FrontEndCafé</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content={description} />
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={`${title} - FrontEndCafé`} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:site" content="@frontendcafe" />
-        <meta
-          name="twitter:image"
-          content="https://frontend.cafe/logo-square.png"
-        />
-        {/* Open Graph */}
-        <meta property="og:title" content={`${title} - FrontEndCafé`} />
-        <meta property="og:description" content={description} />
-        <meta
-          property="og:image"
-          content="https://frontend.cafe/logo-square.png"
-        />
-        {/* Imports */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="antialiased">
-        {preview && <PreviewBanner />}
-        <Header preview={preview} />
-        {children}
-        <Footer />
-      </div>
+      <FecHead title={title} description={description} />
+      {
+        {
+          main: <MainLayout preview={preview}>{children}</MainLayout>,
+          simple: <SimpleLayout preview={preview}>{children}</SimpleLayout>,
+        }[mode]
+      }
     </>
   );
 };
