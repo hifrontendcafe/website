8a8a805f1265e50eb9d21dbd1ab86ce6e981b2e7
Fedeya <elfedeminaya@gmail.com>
2020-12-22 02:52:15 -0300

    feat: better responsive

--- src/components/CMYKCarousel/index.tsx
+++ src/components/CMYKCarousel/index.tsx
@@ -22,11 +22,11 @@ const responsive: ResponsiveType = {
     items: 3,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 600 },
     items: 2,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 600, min: 0 },
     items: 1,
   },
 };
@@ -35,63 +35,54 @@ type CMYKCarouselProps = {
   projects: CMYK[];
 };
 
-const CMYKCarousel: React.FC<CMYKCarouselProps> = ({ projects }) => {
-  const CustomDot: React.FC<DotProps> = ({ onClick, ...rest }) => {
-    const { index, active } = rest;
-    // onMove means if dragging or swiping in progress.
-    // active is provided by this lib for checking if the item is active or not.
-    return (
-      <span
-        className={
-          active
-            ? 'bg-white mx-1 cursor-pointer shadow-sm rounded-md mb-5 z-20 w-2 h-2'
-            : 'bg-white opacity-25 cursor-pointer shadow-sm mx-1 mb-5 rounded-md z-20 w-2 h-2'
-        }
-        onClick={onClick}
-      ></span>
-    );
-  };
+const CMYKCarousel: React.FC<CMYKCarouselProps> = ({ projects }) => (
+  <>
+    <h1 className="absolute ml-16 font-bold mt-10 z-20 text-white text-2xl">
+      CMYK
+    </h1>
 
-  return (
-    <>
-      <h1 className="absolute ml-16 font-bold mt-10 z-20 text-white text-2xl">
-        CMYK
-      </h1>
+    <FecLogo />
+    <Carousel
+      containerClass="flex min-h-screen min-w-screen"
+      itemClass="min-h-screen min-w-screen"
+      ssr
+      showDots
+      arrows={false}
+      draggable
+      swipeable
+      autoPlay
+      infinite
+      customDot={<CustomDot />}
+      responsive={responsive}
+    >
+      {projects.map((project) => (
+        <CMYKItem key={project._id} project={project} />
+      ))}
+    </Carousel>
+  </>
+);
 
-      <FecLogo />
-      <Carousel
-        containerClass="flex min-h-screen min-w-screen"
-        itemClass="min-h-screen min-w-screen"
-        ssr
-        showDots
-        arrows={false}
-        draggable
-        swipeable
-        autoPlay
-        infinite
-        customDot={<CustomDot />}
-        responsive={responsive}
-      >
-        {projects.map((project) => (
-          <CMYKItem key={project._id} project={project} />
-        ))}
-      </Carousel>
-    </>
-  );
-};
+const FecLogo: React.FC = () => (
+  <Link href="/">
+    <a className="absolute right-0 mt-6 mr-16 z-20">
+      <img
+        src="/logo.svg"
+        className="w-16 h-16 p-2 rounded-full"
+        alt="FRONTENDCAFE"
+      />
+    </a>
+  </Link>
+);
 
-const FecLogo: React.FC = () => {
-  return (
-    <Link href="/">
-      <a className="absolute right-0 mt-10 mr-16 z-20">
-        <img
-          src="/logo.svg"
-          className="w-16 h-16 p-2 rounded-full"
-          alt="FRONTENDCAFE"
-        />
-      </a>
-    </Link>
-  );
-};
+const CustomDot: React.FC<DotProps> = ({ onClick, active }) => (
+  <span
+    className={
+      active
+        ? 'bg-white mx-1 cursor-pointer shadow-sm rounded-md mb-5 z-20 w-2 h-2'
+        : 'bg-white opacity-25 cursor-pointer shadow-sm mx-1 mb-5 rounded-md z-20 w-2 h-2'
+    }
+    onClick={onClick}
+  ></span>
+);
 
 export default CMYKCarousel;
