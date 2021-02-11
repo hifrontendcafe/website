5d7ccef66f9667bb595ad93be263535be98f226b
Diego <diego.brunetto2@gmail.com>
2021-02-10 22:13:51 -0300

    add responsive view

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,14 +5,12 @@ import {
   faLinkedin,
   faTwitch,
   faYoutube,
-  faInstagram
+  faInstagram,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Footer: React.FC = () => {
-
    const navItems = [
     { title: 'Unirse a Discord', link: 'https://discord.com/invite/3GC6TJd' },
     { title: 'Proyectos CMYK', link: '/cmyk' },
@@ -21,128 +19,74 @@ const Footer: React.FC = () => {
   ];
 
   return (
-    <footer className="relative h-64 bg-black px-24 py-14 text-white text-xl flex justify-between items-center">
-      <div className='w-2/4 '>
-        <img src='/logo-square.png' className='h-12 w-12 rounded-full'></img>
-        <div className='grid grid-cols-2 font-medium	py-5'>
+    <footer className="relative md:h-64 h-80 bg-black lg:px-32 md:px-20 md:py-8 py-5 text-white lg:text-xl text-md flex flex-col justify-between items-center md:items-start">
+      <img src='/logo-square.png' className='md:h-12 md:w-12 h-10 w-10 rounded-full'></img>
+      <div className='flex flex-col md:flex-row w-full items-center justify-between py-5'>      
+        <div className='grid grid-cols-2 font-medium md:w-2/4 md:pb-3 pb-8'>
           {navItems.map(({link, title}) => (
             <Link href={link} key={link}>
-              <a className="">
+              <a>
                 {title}
               </a>
             </Link>
           ))}
         </div>
-        <p className='font-light '>© Frontendcafé 2021</p>
-      </div>
-      <div className='flex flex-col justify-end w-1/4'>
-        <p className='w-auto font-medium'>Encuentranos en:</p>
-        <div className='flex '>
-          <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-            <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full "
-            >
-              <FontAwesomeIcon icon={faTwitter} size="lg" />
-            </a>
-          </Link>
-          <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-            <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full "
-            >
-              <FontAwesomeIcon icon={ faYoutube} size="lg" />
-            </a>
-          </Link>
-          <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-            <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full "
-            >
-              <FontAwesomeIcon icon={faLinkedin} size="lg" />
-            </a>
-          </Link>
-          <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-            <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full "
-            >
-              <FontAwesomeIcon icon={faTwitch} size="lg" />
-            </a>
-          </Link>
-          <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-            <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full "
-            >
-              <FontAwesomeIcon icon={faGithubAlt} size="lg" />
-            </a>
-          </Link>
-          <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-            <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full "
-            >
-              <FontAwesomeIcon icon={faInstagram} size="lg" />
-            </a>
-          </Link>
-        </div>
-      </div>
-
-      {/* <div className="container mx-auto px-32 py-16 bg-gray-500">
-
-        <div className="flex flex-wrap">
-        <div className="w-full lg:w-6/12 px-4 text-center md:text-left">
-        <div className="flex space-x-1 mt-6 justify-center md:justify-start">
-        <Link href="https://twitter.com/FrontEndCafe">
-        <a
-        target="_blank"
-                  className="grid place-items-center h-10 w-10 rounded-full bg-white text-blue-500 hover:bg-gray-200"
-                  >
-                  <FontAwesomeIcon icon={faTwitter} size="lg" />
-                  </a>
-              </Link>
-              
-              <Link href="http://github.com/frontend-cafe">
+        <div className='flex flex-col md:justify-end justify-center md:w-1/4'>
+          <p className='w-auto md:text-left text-center font-medium'>Encuentranos en:</p>
+          <div className='flex md:w-full w-40'>
+            <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
               <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full bg-white text-gray-900 hover:bg-gray-200"
-              >
-              <FontAwesomeIcon icon={faGithubAlt} size="lg" />
-                </a>
-                </Link>
-                
-                <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
-                <a
                 target="_blank"
-                  className="grid place-items-center h-10 w-10 rounded-full bg-white text-red-700 hover:bg-gray-200"
-                  >
-                  <FontAwesomeIcon icon={faYoutube} size="lg" />
-                  </a>
-                  </Link>
-                  
-              <Link href="https://www.linkedin.com/company/frontendcafe/">
-              <a
-              target="_blank"
-              className="grid place-items-center h-10 w-10 rounded-full bg-white text-blue-700 hover:bg-gray-200"
+                className="grid place-items-center h-10 w-10 rounded-full"
               >
-                  <FontAwesomeIcon icon={faLinkedin} size="lg" />
-                  </a>
-                  </Link>
-                  
-                  <Link href="https://www.twitch.tv/frontendcafe">
-                  <a
-                  target="_blank"
-                  className="grid place-items-center h-10 w-10 rounded-full bg-white text-purple-700 hover:bg-gray-200"
-                  >
-                  <FontAwesomeIcon icon={faTwitch} size="lg" />
-                  </a>
-                  </Link>
-                  </div>
-          </div>          
+                <FontAwesomeIcon icon={faTwitter} size="lg" />
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
+              <a
+                target="_blank"
+                className="grid place-items-center h-10 w-10 rounded-full"
+              >
+                <FontAwesomeIcon icon={ faYoutube} size="lg" />
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
+              <a
+                target="_blank"
+                className="grid place-items-center h-10 w-10 rounded-full"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="lg" />
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
+              <a
+                target="_blank"
+                className="grid place-items-center h-10 w-10 rounded-full"
+              >
+                <FontAwesomeIcon icon={faTwitch} size="lg" />
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
+              <a
+                target="_blank"
+                className="grid place-items-center h-10 w-10 rounded-full"
+              >
+                <FontAwesomeIcon icon={faGithubAlt} size="lg" />
+              </a>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCUdXQMrVjrMMWG5NOZFpHqQ">
+              <a
+                target="_blank"
+                className="grid place-items-center h-10 w-10 rounded-full"
+              >
+                <FontAwesomeIcon icon={faInstagram} size="lg"/>
+              </a>
+            </Link>
           </div>
-      </div>   */}
-          </footer>
+        </div>
+        </div>
+      <p className='font-light'>© Frontendcafé 2021</p>
+    </footer>
   );
 };
 
