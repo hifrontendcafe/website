a94394d88b7ff142a8a3a7d9af00b609095be717
Agustina Marijanovic <agustinam@gmail.com>
2021-02-11 23:09:53 -0300

    changed input order and labels on form, removed hero, added title, fixed responsive layout

--- src/pages/iniciativas/index.tsx
+++ src/pages/iniciativas/index.tsx
@@ -1,4 +1,3 @@
-import Hero from '../../components/Hero';
 import Layout from '../../components/Layout';
 import { postInitiative } from '../../lib/api';
 import { useForm } from 'react-hook-form';
@@ -10,29 +9,30 @@ const InitiativesPage: React.FC = ({}) => {
 
   return (
     <Layout title="Iniciativas">
-      <Hero small title="Iniciativas" />
-      <div className="bg-indigo-100 sm:pt-10 pb-24">
-        <div className=" container mx-auto min-h-screen bg-white overflow-hidden shadow rounded-lg">
-          <div className="border-b border-gray-200 px-4 py-5 sm:px-6">
+      <div className="pb-24 bg-indigo-100 sm:pt-10">
+        <div className="container mx-auto overflow-hidden bg-white rounded-lg shadow ">
+          <div className="px-6 pt-20 border-b border-gray-200 md:py-5 md:px-8">
             <div className="mt-2 md:flex md:items-center md:justify-between">
               <div className="flex-1 min-w-0">
-                <h2 className="text-xl font-bold leading-7 text-primary sm:text-2xl sm:leading-9 sm:truncate">
-                  Iniciativas
-                </h2>
+                <h2 className="text-2xl font-bold leading-7 text-black sm:text-2xl sm:leading-9 sm:truncate">
+                    Reactivistas
+                  </h2>
+                  <h2 className="font-medium leading-7 text-md text-primary sm:leading-9 sm:truncate">
+                    Propuesta de nuevo grupo de estudio
+                  </h2>
               </div>
             </div>
           </div>
-
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="bg-white rounded px-8 pt-6 pb-8 mb-4 w-full grid grid-cols-2 gap-5"
+            className="flex flex-col w-full grid-cols-2 gap-5 px-8 pt-6 pb-8 mb-4 bg-white rounded md:grid"
           >
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">
+              <label className="block mb-2 text-sm font-bold">
                 Nombre del grupo
               </label>
               <input
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
                 name="name"
                 type="text"
                 placeholder="Nombre del grupo"
@@ -41,9 +41,9 @@ const InitiativesPage: React.FC = ({}) => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">Tema</label>
+              <label className="block mb-2 text-sm font-bold">Tema</label>
               <input
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
                 name="topic"
                 type="text"
                 placeholder="Ingresa el tema"
@@ -52,24 +52,11 @@ const InitiativesPage: React.FC = ({}) => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">
-                Material de estudio
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="study-material"
-                type="text"
-                placeholder="Ingresa el material de estudio"
-                required
-                ref={register({ required: true })}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">
+              <label className="block mb-2 text-sm font-bold">
                 Representante del grupo
               </label>
               <input
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
                 name="team-leader"
                 type="text"
                 placeholder="Ingresa tu user de discord"
@@ -78,24 +65,25 @@ const InitiativesPage: React.FC = ({}) => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">
-                Fecha de inicio
+              <label className="block mb-2 text-sm font-bold">
+                Material de estudio
               </label>
               <input
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="date"
-                type="date"
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+                name="study-material"
+                type="text"
+                placeholder="Ingresa el material de estudio"
                 required
                 ref={register({ required: true })}
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">
+              <label className="block mb-2 text-sm font-bold">
                 Estilo de reuniones
               </label>
               <textarea
                 rows={5}
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
                 name="meeting-type"
                 placeholder="Ingresa la dinamica que se va a utilizar en su grupo de estudio"
                 required
@@ -103,32 +91,37 @@ const InitiativesPage: React.FC = ({}) => {
               ></textarea>
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-bold mb-2">
+              <label className="block mb-2 text-sm font-bold">
                 Plan de estudio
               </label>
               <textarea
                 rows={5}
-                className="appearance-none border rounded w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
                 name="plan"
                 placeholder="Ingresa como va a ser el plan de estudio"
                 required
                 ref={register({ required: true })}
               ></textarea>
             </div>
-
-            <div className="pt-5">
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-bold">
+                Fecha tentativa de inicio
+              </label>
+              <input
+                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+                name="date"
+                type="date"
+                required
+                ref={register({ required: true })}
+              />
+            </div>
+            <div className="pt-8">
               <div className="flex justify-end">
                 <button
-                  type="button"
-                  className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Cancelar
-                </button>
-                <button
                   type="submit"
-                  className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primarydark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-primarydark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Enviar
+                  Enviar propuesta de grupo
                 </button>
               </div>
             </div>
