8d20f1057ea6a97e0f06246835275dae1e12533a
Hernan Case <her.case@live.com.ar>
2021-02-25 00:59:25 -0300

    added Nickname tooltip.

--- src/components/reactivistas/CreateGroupForm/index.tsx
+++ src/components/reactivistas/CreateGroupForm/index.tsx
@@ -7,6 +7,7 @@ const CreateGroupForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
+  const [showHelp, setShowHelp] = useState<boolean>(false);
 
   const onSubmit = async (data: ReactGroup) => {
     setIsLoading(true);
@@ -51,14 +52,60 @@ const CreateGroupForm = () => {
       </div>
       <div className="mb-4">
         <label className="block mb-2 text-sm font-bold">User de Discord*</label>
-        <input
-          className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
-          name="teamCaptain.id"
-          type="text"
-          placeholder="Ingresa tu usuario de Discord"
-          required
-          ref={register({ required: true })}
-        />
+        <div className="relative">
+          <input
+            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+            name="teamCaptain.id"
+            type="text"
+            placeholder="Ingresa tu usuario de Discord"
+            required
+            pattern="/(.*)#(\d{4})/g"
+            ref={register({ required: true })}
+          />
+
+          <div
+            onMouseOver={() => setShowHelp(true)}
+            onMouseLeave={() => setShowHelp(false)}
+            className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+          >
+            <svg
+              className="h-5 w-5 text-primary cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+
+          <div
+            className={`absolute bg-gradient-to-r bg-primary text-white px-4 py-2 rounded flex items-center transition-all duration-150 ${
+              showHelp ? 'opacity-100' : 'opacity-0'
+            }`}
+            style={{ right: '0px', bottom: '-50' }}
+          >
+            <div
+              className="bg-primary h-3 w-3 absolute"
+              style={{ top: '-6px', right: '16px', transform: 'rotate(45deg)' }}
+            />
+            <div className="w-64 leading-none p-1">
+              <p className="text-xs">
+                Para encontrar tu Discord ID solo debes hacer click en tu nombre
+                en la aplicacion. El formato estandar es del tipo
+                frontendcafe#5239
+              </p>
+              <img
+                className="w-full rounded-lg mt-2"
+                src="/img/nickname-help.png"
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="mb-4">
         <label className="block mb-2 text-sm font-bold">Tema*</label>
