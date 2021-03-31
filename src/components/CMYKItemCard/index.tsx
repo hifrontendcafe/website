bc47a5ae22be352c4e3637e96156a7aee5c4b34f
Sebastián Córdoba <sebascordobaa@hotmail.com>
2021-03-30 23:10:18 -0300

    Cards not finished

--- src/components/CMYKItemCard/index.tsx
+++ src/components/CMYKItemCard/index.tsx
@@ -9,10 +9,13 @@ type CMYKItemProps = {
 };
 
 const CMYKItemCard: React.FC<CMYKItemProps> = ({ project, index }) => {
+  const btnStyles = {
+    color: project.color,
+  };
   return (
     <div
-      className={`max-w-md rounded overflow-hidden ${
-        index % 2 === 0 ? 'mb-10' : 'mt-10'
+      className={`max-w-md rounded overflow-hidden mb-5 flex flex-col ${
+        index % 2 === 0 ? 'md:mb-5' : 'md:mt-5'
       }`}
     >
       <img
@@ -28,18 +31,24 @@ const CMYKItemCard: React.FC<CMYKItemProps> = ({ project, index }) => {
           <div className="font-semibold text-2xl mb-2">{project.name}</div>
           <p className="text-sm">{project.description}</p>
         </div>
-        <div className="px-6 py-4 flex flex-col border-l-2 my-4">
-          <Link href={project.github}>
-            <a className="font-semibold mr-3" target="_blank">
-              Github
-            </a>
-          </Link>
+        <div className="px-3 py-4 flex flex-col border-l-2 my-4">
           <Link href={project.demo}>
             <a className="font-semibold mr-3" target="_blank">
               DEMO
             </a>
           </Link>
-          <button>Ver más</button>
+          <Link href={project.github}>
+            <a className="font-semibold mr-3" target="_blank">
+              GITHUB
+            </a>
+          </Link>
+          <a
+            href="#"
+            className="btn bg-white whitespace-no-wrap"
+            style={btnStyles}
+          >
+            Ver más
+          </a>
         </div>
       </div>
     </div>
--- src/pages/cmyk/index.tsx
+++ src/pages/cmyk/index.tsx
@@ -1,5 +1,6 @@
 import { InferGetStaticPropsType } from 'next';
 import CMYKItemCard from '../../components/CMYKItemCard';
+import JoinSection from '../../components/JoinSection';
 
 import Layout from '../../components/Layout';
 
@@ -22,12 +23,17 @@ const CMYK: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
       mode="main"
       preview={preview}
     >
-      <div className="py-20">
+      <div className="pt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="mt-2 text-4xl sm:text-5xl leading-snug font-extrabold tracking-tight text-gray-900">
               Proyectos hechos con{' '}
-              <img src="/img/Heart.svg" className="inline" alt="heart" />
+              <img
+                src="/icons/hearth.svg"
+                width="50px"
+                className="inline"
+                alt="heart"
+              />
               <br /> por la comunidad
             </h2>
             <p className="mt-4 max-w-3xl text-lg lg:mx-auto">
@@ -37,14 +43,37 @@ const CMYK: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
             </p>
           </div>
         </div>
-        {/* <img src="/img/CMYK-bg.svg" alt="background" className="mt-10 w-full" /> */}
+        <img
+          src="/img/CMYK-bg.svg"
+          alt="background"
+          className="mt-5 w-full absolute"
+        />
         <div
-          className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 justify-items-center"
-          style={{ backgroundImage: '/img/CMYK-bg.svg' }}
+          className="w-full h-full mt-24"
+          style={{ backgroundColor: '#F5F9FF' }}
         >
-          {projects.map((project, index) => (
-            <CMYKItemCard key={project._id} project={project} index={index} />
-          ))}
+          <div className="max-w-6xl mx-auto p-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 relative z-10 mt-20">
+            {projects.map((project, index) => (
+              <CMYKItemCard key={project._id} project={project} index={index} />
+            ))}
+          </div>
+          <div className="text-center py-10">
+            <h2 className="mb-8 text-xl font-semibold tracking-tight text-gray-900">
+              El siguiente puede ser el tuyo{' '}
+              <img
+                src="/icons/hearth.svg"
+                className="inline"
+                alt="heart"
+                width="30px"
+              />
+            </h2>
+            <a
+              href="https://discord.gg/frontendcafe"
+              className="btn mt-1 btn-secondary py-3 px-6"
+            >
+              Súmate a Discord
+            </a>
+          </div>
         </div>
       </div>
     </Layout>
