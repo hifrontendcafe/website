0ef5ff41b1f5307b4517319ee12569487479618e
Hernan Casé <hernan.case@globant.com>
2021-04-01 11:08:47 -0300

    fix Arrow sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,7 @@ const Index: React.FC<IndexProps> = ({ preview = false, settings, cards }) => {
 };
 
 const Featured = ({ cards }) => (
-  <div className="flex flex-col mb-12">
+  <div className="flex flex-col mb-24">
     <div className="flex flex-col justify-center m-auto mt-20 items-center  text-center lg:w-2/3">
       <h1 className="lg:text-5xl md:text-4xl text-xl font-extrabold mb-5">
         ¡Descubre lo que tenemos para ti!
