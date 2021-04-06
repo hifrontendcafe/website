f485c420dc0f4ab86fc439fbcab2249417c081e5
Hernan Case <her.case@live.com.ar>
2021-04-06 20:08:41 -0300

    fix TopicBadge colors

--- src/components/TopicBadge/index.tsx
+++ src/components/TopicBadge/index.tsx
@@ -8,10 +8,13 @@ const TopicBadge: React.FC<TopicBadgeProps> = ({ topic }) => {
 
   // Color by type of topic.
   const work = ['Orientación / CV'];
-  const frontend = ['Frontend','Diseño UX-UI'];
+  const frontend = ['Frontend', 'Diseño UX-UI'];
   const backend = ['Backend'];
   const data = ['Anilitica Web / App', 'Data Science / Data Engineer'];
-  const programming = ['Diseño y Arquitectura de Software', 'Intro a la Programación' ];
+  const programming = [
+    'Diseño y Arquitectura de Software',
+    'Intro a la Programación',
+  ];
   const languages = ['Inglés'];
   const tools = ['Git'];
 
@@ -21,33 +24,33 @@ const TopicBadge: React.FC<TopicBadgeProps> = ({ topic }) => {
   }
 
   if (data.includes(topic)) {
-    textColor = 'text-indigo-800';
-    bgColor = 'bg-indigo-200';
+    textColor = 'text-pink-800';
+    bgColor = 'bg-pink-200';
   }
 
   if (backend.includes(topic)) {
-    textColor = 'text-teal-800';
-    bgColor = 'bg-teal-200';
-  }
-
-  if (work.includes(topic)) {
     textColor = 'text-green-800';
     bgColor = 'bg-green-200';
   }
 
+  if (work.includes(topic)) {
+    textColor = 'text-blue-800';
+    bgColor = 'bg-blue-200';
+  }
+
   if (programming.includes(topic)) {
-    textColor = 'text-yellow-800';
-    bgColor = 'bg-yellow-200';
+    textColor = 'text-indigo-800';
+    bgColor = 'bg-indigo-200';
   }
 
   if (tools.includes(topic)) {
-    textColor = 'text-purple-800';
-    bgColor = 'bg-purple-200';
+    textColor = 'text-violet-800';
+    bgColor = 'bg-violet-200';
   }
 
   if (languages.includes(topic)) {
-    textColor = 'text-orange-800';
-    bgColor = 'bg-orange-200';
+    textColor = 'text-yellow-800';
+    bgColor = 'bg-yellow-200';
   }
 
   return (
