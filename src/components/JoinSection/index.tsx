0fb553b5549434d4e31dd7015ee8f993a65754d7
Agus Mar <44821883+agusmar@users.noreply.github.com>
2021-04-16 12:48:44 -0300

    fixed navbar breakpoint for tablet devices, fixed English hero section (#126)

--- src/components/JoinSection/index.tsx
+++ src/components/JoinSection/index.tsx
@@ -1,13 +1,13 @@
 const JoinSection: React.FC = () => (
-  <section className="flex flex-col items-center justify-center gap-5 text-center my-24 md:my-32 container mx-auto">
-    <span className="inline-flex gap-2 items-center relative text-2xl md:text-3xl font-semibold">
+  <section className="container flex flex-col items-center gap-5 mb-24">
+    <span className="relative inline-flex items-center gap-2 text-xl font-semibold md:text-3xl">
       ¿Estás listo para unirte?
       <img className="w-8 h-8" src="/icons/hearth.svg" />
     </span>
     <a
       target="_blank"
       href="https://discord.gg/frontendcafe"
-      className="btn mt-1 btn-secondary"
+      className="mt-1 btn btn-secondary"
     >
       Súmate a Discord
     </a>
