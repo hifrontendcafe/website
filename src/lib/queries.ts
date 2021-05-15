62537e398f7dca8e582ab82d0b0dba29d31be377
Fedeya <elfedeminaya@gmail.com>
2021-05-15 00:43:22 -0300

    feat: description i18n

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -3,7 +3,7 @@ import { groq } from 'next-sanity';
 export const settingsQuery = groq`
   *[_type == "settings"][0]{
     title,
-    description,
+    "description": description[$locale],
     "menu": menu[].[$locale],
     logo,
     heroBackground,
