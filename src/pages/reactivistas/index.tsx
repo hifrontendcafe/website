3910fb7bc3fe7a045390fb901fb45ac386287291
Juan Romero <juanprmendoza96@gmail.com>
2021-05-20 01:31:36 -0500

    Feat: add hero to Reactivistas

--- src/pages/reactivistas/index.tsx
+++ src/pages/reactivistas/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Link from 'next/link';
 
@@ -14,6 +14,8 @@ import CreateGroupForm from '../../components/reactivistas/CreateGroupForm';
 import GroupRequirementsModal from '../../components/reactivistas/GroupRequirementsModal';
 
 import { getLayout } from '@/utils/get-layout';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ReactGroupPage: React.FC<InferGetStaticPropsType<
   typeof getStaticProps
@@ -27,72 +29,85 @@ const ReactGroupPage: React.FC<InferGetStaticPropsType<
 
   return (
     <Layout title="Iniciativas">
-      <Hero title="Reactivistas" />
-      <div className="pb-24 bg-indigo-100 sm:pt-10 lg:mt-0 mt-24">
-        <Link href="/docs/guia-reactivistas">
-          <a
-            className="container mx-auto flex justify-center mx-10 text-white text-center bg-primary border-0 py-2  px-6
-            focus:outline-none hover:bg-primarydark rounded text-lg font-bold"
-          >
-            ¿De qué se trata Reactivistas?
-          </a>
-        </Link>
-        <div className="container mx-auto m-10 p-10 bg-white shadow">
-          <h1 className="text-2xl font-bold leading-7 text-black sm:text-2xl sm:leading-9 sm:truncate">
-            Súmate a los grupos que están comenzando
-          </h1>
-          <div className="flex flex-wrap md:m-10 m-2">
-            {groups?.map((group: ReactGroup) => {
-              const [infoModalOpen, setInfoModalOpen] = useState(false);
-              return (
-                <div
-                  key={group.name}
-                  className="flex flex-col flex-auto rounded-md shadow-lg md:mx-5 mx-1 my-5 md:p-10 p-5"
-                >
-                  <div>
-                    <div className="flex justify-between items-baseline">
-                      <h3 className="font-medium leading-7 text-lg text-primary mb-5 mr-5 sm:leading-9 sm:truncate">
-                        ⚛ {group.name}
-                      </h3>
-                      <button
-                        onClick={() => setInfoModalOpen(true)}
-                        className="text-xs uppercase font-bold text-blue-600 focus:outline-none"
-                      >
-                        + Info
-                      </button>
-                    </div>
-                    <ul className="mb-6">
-                      <li key={group.topic} className="mb-1 font-medium">
-                        <span className="font-bold">Tema:</span> {group.topic}
-                      </li>
-                      <li className="font-medium mb-4">
-                        <span className="font-bold">Fecha de inicio:</span>{' '}
-                        {group.startDate}
-                      </li>
-                      <li className="font-medium text-primary">
-                        <a href={group.studyMaterial} className="text-gray-600">
-                          📚 Material de estudio
-                        </a>
-                      </li>
-                    </ul>
-                    {group.participants && group.participants.length >= 10 ? (
-                      <div className="font-md text-md text-red-500">
-                        Grupo lleno
-                      </div>
-                    ) : (
-                      <AddParticipantForm group={group} />
-                    )}
-                  </div>
-
-                  <GroupInfoModal
-                    open={infoModalOpen}
-                    onClose={() => setInfoModalOpen(false)}
-                    group={group}
-                  />
-                </div>
-              );
-            })}
+      <div className="pt-20">
+        <div className="max-w-7xl mx-auto px-6 sm:px-6 lg:px-8">
+          <div className="text-left lg:pl-28">
+            <h2 className="title mt-2 leading-snug tracking-tight">
+              Reactivistas&nbsp;
+            </h2>
+            <p className="mt-4 max-w-3xl text-lg">
+              Reactivistas es una iniciativa que promueve el estudio de React en
+              grupos auto-organizados por integrantes de la comunidad.
+              <br />
+              Si participas podrás intercambiar ideas con miembros de la
+              comunidad, y acceder a las Office Hours, son reuniones con nuestro
+              staff para exponer tus dudas y realizar consultas
+            </p>
+            <span className="cursor-pointer text-primary flex mt-5">
+              <a href="https://frontend.cafe/docs/guia-reactivistas">
+                Conocé más sobre la iniciativa&nbsp;
+              </a>
+              <FontAwesomeIcon icon={faExternalLinkAlt} width="16px" />
+            </span>
           </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto m-10 p-10 bg-white shadow">
+        <h1 className="text-2xl font-bold leading-7 text-black sm:text-2xl sm:leading-9 sm:truncate text-justify	  ">
+          Súmate a los grupos que están comenzando
+        </h1>
+        <div className="flex flex-wrap md:m-10 m-2">
+          {groups?.map((group: ReactGroup) => {
+            const [infoModalOpen, setInfoModalOpen] = useState(false);
+            return (
+              <div
+                key={group.name}
+                className="flex flex-col flex-auto rounded-md shadow-lg md:mx-5 mx-1 my-5 md:p-10 p-5"
+              >
+                <div>
+                  <div className="flex justify-between items-baseline">
+                    <h3 className="font-medium leading-7 text-lg text-primary mb-5 mr-5 sm:leading-9 sm:truncate">
+                      ⚛ {group.name}
+                    </h3>
+                    <button
+                      onClick={() => setInfoModalOpen(true)}
+                      className="text-xs uppercase font-bold text-blue-600 focus:outline-none"
+                    >
+                      + Info
+                    </button>
+                  </div>
+                  <ul className="mb-6">
+                    <li key={group.topic} className="mb-1 font-medium">
+                      <span className="font-bold">Tema:</span> {group.topic}
+                    </li>
+                    <li className="font-medium mb-4">
+                      <span className="font-bold">Fecha de inicio:</span>{' '}
+                      {group.startDate}
+                    </li>
+                    <li className="font-medium text-primary">
+                      <a href={group.studyMaterial} className="text-gray-600">
+                        📚 Material de estudio
+                      </a>
+                    </li>
+                  </ul>
+                  {group.participants && group.participants.length >= 10 ? (
+                    <div className="font-md text-md text-red-500">
+                      Grupo lleno
+                    </div>
+                  ) : (
+                    <AddParticipantForm group={group} />
+                  )}
+                </div>
+
+                <GroupInfoModal
+                  open={infoModalOpen}
+                  onClose={() => setInfoModalOpen(false)}
+                  group={group}
+                />
+              </div>
+            );
+          })}
         </div>
 
         <div className="container mx-auto overflow-hidden bg-white rounded-lg shadow ">
