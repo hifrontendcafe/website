fab2a37198c1de0a1a2c20737dfac3f48a9dd118
Hernan Case <her.case@live.com.ar>
2021-05-25 21:28:42 -0300

    add spacing between tech and clear button

--- src/pages/comunidad/index.tsx
+++ src/pages/comunidad/index.tsx
@@ -60,7 +60,7 @@ const ProfilesPage: React.FC<PostsPageProps> = ({ profiles, preview }) => {
         <div className="px-4 py-5 border-b border-gray-200 sm:px-6 md:flex md:justify-between">
           <div className="mb-2 font-bold leading-7 md:text-xl text-primary md:mb-0">
             {filter ? (
-              <span className="flex gap-1 items-center">
+              <span className="flex gap-2 items-center">
                 {filter.toUpperCase()}
                 <button
                   type="button"
