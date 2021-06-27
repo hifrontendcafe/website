4e11da9a2f6611ca0d80b1e304ca754e1839f886
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-06-27 15:28:02 -0300

    feat: add new component and test push this changes

--- src/components/MentorshipStep/index.tsx
+++ src/components/MentorshipStep/index.tsx
@@ -4,21 +4,21 @@ interface MentorStepProps {
 }
 
 const evenStep = {
-    container: 'relative flex items-center w-96 h-24 bg-green-400 px-6 my-6 ml-20 rounded-bl-full rounded-tl-full',
+    container: 'relative flex items-center w-96 h-24 bg-primary px-6 my-6 ml-20 rounded-bl-full rounded-tl-full',
     circle: 'absolute -right-16 -top-2.5 w-28 h-28 bg-white rounded-full font-bold align-middle text-center',
-    step: 'text-green-400 text-7xl m-0',
-    text: 'w-72 pl-2 text-xs text-white font-semibold'
+    step: 'text-primary text-7xl m-0',
+    text: 'w-72 pl-2 text-xs text-white font-semibold font-sans'
 }
 
 const oddStep = {
     container: 'relative flex justify-end items-center w-96 h-24 bg-white px-6 ml-14 my-6 rounded-br-full rounded-tr-full',
-    circle: 'absolute -left-14 -top-2.5 w-28 h-28 bg-green-400 rounded-full font-bold align-middle text-center',
+    circle: 'absolute -left-14 -top-2.5 w-28 h-28 bg-primary rounded-full font-bold align-middle text-center',
     step: 'text-white text-7xl m-0',
-    text: 'w-72 pl-2 text-xs text-blue-900 font-semibold'
+    text: 'w-72 pl-2 text-xs text-blue-900 font-semibold font-sans'
 }
 
 const MentorshipStep: React.FC<MentorStepProps> = ({ number, textStep }) => {
-    const isEven = number % 2 === 0 ? true : false
+    const isEven = number % 2 === 0
     return (
         <div className={isEven ? evenStep.container : oddStep.container}
             style={{boxShadow: '0 0 15px 2px rgba(0, 0, 0, 0.1)'}}>
