77735ca4cbcac0c7f24de5ed0daa8d341ef1b84f
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-06-30 12:05:51 -0300

    Redesign "Mentorias": Mentorship steps finished

--- src/components/MentorshipStep/index.tsx
+++ src/components/MentorshipStep/index.tsx
@@ -4,17 +4,17 @@ interface MentorStepProps {
 }
 
 const evenStep = {
-    container: 'relative flex items-center w-96 h-24 bg-primary px-6 my-6 ml-20 rounded-bl-full rounded-tl-full',
-    circle: 'absolute -right-16 -top-2.5 w-28 h-28 bg-white rounded-full font-bold align-middle text-center',
+    container: 'relative flex items-center w-64 md:w-96 h-24 bg-primary px-6 my-6 mr-16 md:mr-0 rounded-bl-full rounded-tl-full',
+    circle: 'absolute -right-16 -top-2.5 w-28 h-28 bg-white rounded-full font-sans font-black align-middle text-center',
     step: 'text-primary text-7xl m-0',
-    text: 'w-72 pl-2 text-xs text-white font-semibold font-sans'
+    text: 'w-48 md:w-72 pl-2 text-xs text-white font-semibold font-sans leading-4'
 }
 
 const oddStep = {
-    container: 'relative flex justify-end items-center w-96 h-24 bg-white px-6 ml-14 my-6 rounded-br-full rounded-tr-full',
-    circle: 'absolute -left-14 -top-2.5 w-28 h-28 bg-primary rounded-full font-bold align-middle text-center',
+    container: 'relative flex justify-end items-center w-64 md:w-96 h-24 bg-white px-2 md:px-6 my-6 ml-14 md:ml-0 rounded-br-full rounded-tr-full',
+    circle: 'absolute -left-14 -top-2.5 w-28 h-28 bg-primary rounded-full font-sans font-black align-middle text-center',
     step: 'text-white text-7xl m-0',
-    text: 'w-72 pl-2 text-xs text-blue-900 font-semibold font-sans'
+    text: 'w-48 md:w-72 pl-1 text-xs text-blue-900 font-semibold font-sans leading-4'
 }
 
 const MentorshipStep: React.FC<MentorStepProps> = ({ number, textStep }) => {
--- src/pages/mentorias/index.tsx
+++ src/pages/mentorias/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { GetStaticProps } from 'next';
 
 import MentorList from '../../components/MentorList';
@@ -54,13 +55,38 @@ const MentorshipsPage: React.FC<MentorshipsPageProps> = ({
       preview={preview}
     >
       <MentorshipsHero />
-      <div style={{backgroundColor: '#f7f7f7'}}>
-        <div className="container mx-auto flex px-5 py-16 flex-col items-start0">
+      <div className="py-16 flex flex-col justify-center lg:flex-row" style={{backgroundColor: '#f7f7f7'}}>
+        <div className="container mx-auto flex px-5 pb-4 flex-col items-center lg:items-start md:px-20">
+          <h2 className="inline-block lg:hidden text-3xl lg:text-5xl font-black text-center pb-6 text-gray-800">
+            ¿Por dónde empiezo?
+          </h2>
           {
             infoStep.map((s, key) => {
               return <MentorshipStep number={s.number} textStep={s.textStep} key={key}/>
             })
           }
+        </div>
+        <div className="container mx-auto px-10 pt-4 flex flex-col items-center lg:items-end text-center lg:text-right">
+          <div className="pb-10 text-gray-800">  
+            <h2 className="hidden lg:inline-block text-3xl lg:text-5xl font-black w-72">¿Por dónde empiezo?</h2>
+            <p>
+              Las mentorías son exclusivas para miembros de <strong className="font-bold">FRONTEND</strong>
+              <strong className="font-semibold">CAFE</strong> ya que suceden en nuestro servidor de Discord.
+              Asegúrate de poder compromenterte con el mentor antes de hacer la reserva, ¡y
+              recuerda ser puntual!
+              <br /><br />
+              Si tienes más dudas puedes hacerlas dentro del canal de #📚︱consultas-mentorias.
+            </p>
+          </div>
+          <Link href="https://discord.gg/frontendcafe">
+            <a
+              target="_blank"
+              className="bg-secondary hover:bg-secondarydark text-white text-center font-bold py-2 px-4 rounded my-3 block w-80"
+              style={{ transition: 'all .15s ease' }}
+            >
+              Súmate a Discord
+            </a>
+          </Link>
         </div>
       </div>
       <MentorList topics={topics} mentors={mentors} />
