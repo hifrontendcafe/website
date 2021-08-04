92c01bae223f1b6b30da87ec0f205ade71d976bb
giancarlol <giancarlo.brusca@mercadolibre.com>
2021-08-03 21:20:26 -0300

    delete emailjs import from reactivistas/index.tsx

--- src/pages/reactivistas/index.tsx
+++ src/pages/reactivistas/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
 import { getApprovedReactGroups } from '../../lib/api';
