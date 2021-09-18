7a6d1d88ba358d081090c0aa152dc597ec36fae9
Santiago Santana <santiago.santana@naturalth.it>
2021-09-18 00:20:24 -0300

    fix(profiles): show randomized profiles

---
+++ src/lib/shuffle.ts
@@ -0,0 +1,15 @@
+/**
+ * shuffle in place (Fisher-Yates)
+ */
+export function shuffle(array: any[]): void {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
