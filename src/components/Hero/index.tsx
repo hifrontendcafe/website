074ee26b97310e1b0ef68c236be8abaccad1b62e
Santiago Santana <santiago.santana@naturalth.it>
2021-10-10 02:23:51 -0300

    refactor(hero): remove unused code

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,17 +1,10 @@
-import Link from 'next/link';
 import { motion } from 'framer-motion';
-import { imageBuilder } from '../../lib/sanity';
-import { useSettings } from '@/lib/settings';
 
 interface HeroProps {
   title?: string;
 }
 
 const Hero: React.FC<HeroProps> = ({ title }) => {
-  const { heroBackground } = useSettings();
-
-  const bg = imageBuilder.image(heroBackground).width(1280).url();
-
   return (
     <div className="container relative px-8 py-32 mx-auto bg-white bg-opacity-0">
       <div className="flex items-center justify-center h-full text-white bg-center bg-cover md:justify-around">
@@ -54,35 +47,6 @@ const Hero: React.FC<HeroProps> = ({ title }) => {
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
-
-type CounterSquareProps = {
-  big: string;
-  text: string;
-  color?: string;
-  bgColor?: string;
-};
-
-const CounterSquare: React.FC<CounterSquareProps> = ({
-  big = '10',
-  text = 'Default',
-  color = 'bg-green-400',
-  bgColor = 'bg-green-500',
-}) => {
-  return (
-    <div
-      className={`flex justify-center items-center relative w-24 lg:w-32 h-20 lg:h-24 rounded-lg ${bgColor}`}
-    >
-      <div
-        style={{ transform: 'rotate(-15deg)' }}
-        className={`z-10 absolute top-0 left-0 w-24 lg:w-32 h-20 lg:h-24 rounded-lg ${color}`}
-      ></div>
-      <div className="z-20 font-bold text-center text-white lg:text-left">
-        <p className="text-2xl lg:text-3xl">{big}</p>
-        <p className="text-md">{text}</p>
       </div>
     </div>
   );
