81c1509e275525d80a83cc5844895b5765a0bef7
Santiago Santana <santiago.santana@naturalth.it>
2021-10-10 01:59:38 -0300

    refactor(layout): remove layout without header and footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,50 +4,31 @@ import PreviewBanner from '../PreviewBanner';
 import FecHead from '../FecHead';
 
 type LayoutProps = {
-  mode?: 'main' | 'simple';
   title?: string;
   description?: string;
   ogImage?: string;
   preview?: boolean;
 };
 
-type LayoutPreviewProps = {
-  preview?: boolean;
-};
-
-const MainLayout: React.FC<LayoutPreviewProps> = ({ preview, children }) => (
-  <div className="w-full bg-gray-900">
-    <div className="absolute w-3/4 transform -translate-x-1/2 -translate-y-1/3 bg-gradient-to-b from-ellipseBlue via-ellipseBlue to-transparent left-1/2 h-3/5 ellipse filter blur-3xl opacity-70"></div>
-    <div className="z-10">
-      {preview && <PreviewBanner />}
-      <Header preview={preview} />
-      {children}
-      <Footer />
-    </div>
-  </div>
-);
-
-const SimpleLayout: React.FC<LayoutPreviewProps> = ({ preview, children }) => (
-  <div className="antialiased">
-    {preview && <PreviewBanner />}
-    {children}
-  </div>
-);
-
 const Layout: React.FC<LayoutProps> = ({
-  mode = 'main',
   title,
   description,
   ogImage,
   preview = false,
   children,
 }) => {
-  const SelectedLayout = mode === 'simple' ? SimpleLayout : MainLayout;
-
   return (
     <>
       <FecHead title={title} description={description} ogImage={ogImage} />
-      <SelectedLayout preview={preview}>{children}</SelectedLayout>
+      <div className="w-full bg-gray-900">
+        <div className="absolute w-3/4 transform -translate-x-1/2 -translate-y-1/3 bg-gradient-to-b from-ellipseBlue via-ellipseBlue to-transparent left-1/2 h-3/5 ellipse filter blur-3xl opacity-70"></div>
+        <div className="z-10">
+          {preview && <PreviewBanner />}
+          <Header preview={preview} />
+          {children}
+          <Footer />
+        </div>
+      </div>
     </>
   );
 };
