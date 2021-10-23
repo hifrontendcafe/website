edbb91c216d4e83548eb44e34c5ac88f7739a44c
Javier Rodriguez <javicerodriguez@gmail.com>
2021-10-22 23:10:05 -0300

    feat: add paragraph card class & delete comments

--- src/components/CMYKItemCard/index.tsx
+++ src/components/CMYKItemCard/index.tsx
@@ -9,7 +9,6 @@ type CMYKItemProps = {
 
 const CMYKItemCard: React.FC<CMYKItemProps> = ({ project }) => {
   return (
-    // <div className="p-5 w-96 bg-gray-800 border border-gray-500 rounded-lg flex flex-col justify-between overflow-hidden">
     <div className="p-5 w-72 lg:w-96 bg-gray-800 border border-gray-500 rounded-lg flex flex-col justify-between overflow-hidden">
       <div>
         <img
@@ -18,16 +17,13 @@ const CMYKItemCard: React.FC<CMYKItemProps> = ({ project }) => {
           alt="Project image"
         />
         <h1 className="cards-title my-2">{project.name}</h1>
-        <p className="text-gray-200 text-sm font-light">
-          {project.description}
-        </p>
+        <p className="cards-paragraph">{project.description}</p>
       </div>
       <div className="w-full flex justify-between mt-5">
         <Link href={project.demo}>
           <a
             target="_blank"
             rel="noopener noreferrer"
-            // className=" font-sans font-semibold text-sm bg-gray-50 rounded-lg"
             className="btn px-9 lg:px-14 btn-primary"
           >
             Demo
@@ -37,7 +33,6 @@ const CMYKItemCard: React.FC<CMYKItemProps> = ({ project }) => {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            // className="py-2 px-14 font-sans font-semibold text-sm text-gray-50 border border-gray-50 rounded-lg"
             className="btn px-9 lg:px-14 btn-secondary"
           >
             GitHub
