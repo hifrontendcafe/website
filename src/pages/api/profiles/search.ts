e45f7890faaa63ad90bc4ddd70cee95b3a4c465f
giancarlol <giancarlo.brusca@mercadolibre.com>
2021-11-12 00:56:27 -0300

    include fullstacks in fe and be result

--- src/pages/api/profiles/search.ts
+++ src/pages/api/profiles/search.ts
@@ -3,6 +3,10 @@ import { ProfileFilters } from '@/lib/types';
 import { NextApiResponse } from 'next';
 import { findProfiles } from '@/lib/prisma-queries';
 
+const fullstackRoleId = 'ed07741b-f317-4fb9-9a76-7bacf95d7e5e';
+const frontendRoleId = '348d3725-30a4-458d-b299-2d2d9381795c';
+const backendRoleId = '391270dd-4121-4f80-b6da-811c9a0b84cf';
+
 type Insensitive = 'insensitive';
 const mode: Insensitive = 'insensitive';
 
@@ -20,7 +24,31 @@ function makeFilters({
   const retval: ProfileWhereInput = {};
 
   if (roleId) {
-    retval.role = { id: roleId };
+    if (roleId === frontendRoleId) {
+      retval.role = {
+        OR: [
+          {
+            id: frontendRoleId,
+          },
+          {
+            id: fullstackRoleId,
+          },
+        ],
+      };
+    } else if (roleId === backendRoleId) {
+      retval.role = {
+        OR: [
+          {
+            id: backendRoleId,
+          },
+          {
+            id: fullstackRoleId,
+          },
+        ],
+      };
+    } else {
+      retval.role = { id: roleId };
+    }
   }
 
   if (available) {
