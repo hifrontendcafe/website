42568edd655bdfc00ea0731fa82a39072b3377d8
Santiago Santana <santana.santiago@gmail.com>
2022-01-09 19:05:52 -0300

    feat: add feature page for reactivistas form

--- src/pages/reactivistas/index.tsx
+++ src/pages/reactivistas/index.tsx
@@ -31,87 +31,90 @@ const ReactGroupPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> =
           paragraph="Grupos auto-organizados por integrantes de la comunidad para aprender React.js con pares y con ayuda de mentores"
           cta="https://frontend.cafe/docs/guia-reactivistas"
         />
-        <div>
-          {groups.length > 0 && (
-            <>
-              <h1 className="text-2xl font-bold leading-7 text-justify text-primary sm:text-2xl sm:leading-9 sm:truncate ">
-                Súmate a los grupos que están comenzando
-              </h1>
-              <div className="flex flex-wrap m-2 md:m-10 text-primary">
-                {groups?.map((group: ReactGroup) => {
-                  return (
-                    <div
-                      key={group.name}
-                      className="flex flex-col flex-auto p-5 mx-1 my-5 rounded-md md:mx-5 md:p-10 border-2 border-gray-500 bg-coolGray-900"
-                    >
-                      <div>
-                        <div className="flex items-baseline justify-between ">
-                          <h3 className="mb-5 mr-5 text-lg font-medium leading-7 sm:leading-9 sm:truncate">
-                            ⚛ {group.name}
-                          </h3>
-                          <button
-                            onClick={() => setInfoModalOpen(true)}
-                            className="text-xs font-bold text-blue-600 uppercase focus:outline-none"
-                          >
-                            + Info
-                          </button>
-                        </div>
-                        <ul className="mb-6">
-                          <li key={group.topic} className="mb-1 font-medium">
-                            <span className="font-bold">Tema:</span>{' '}
-                            {group.topic}
-                          </li>
-                          <li className="mb-4 font-medium">
-                            <span className="font-bold">Fecha de inicio:</span>{' '}
-                            {new Date(group.startDate).toLocaleDateString()}
-                          </li>
-                          <li className="font-medium text-primary">
-                            <a
-                              href={group.studyMaterial}
-                              className="text-informational"
+        {data.isEnabled && (
+          <di v>
+            {groups.length > 0 && (
+              <>
+                <h1 className="text-2xl font-bold leading-7 text-justify text-primary sm:text-2xl sm:leading-9 sm:truncate ">
+                  Súmate a los grupos que están comenzando
+                </h1>
+                <div className="flex flex-wrap m-2 md:m-10 text-primary">
+                  {groups?.map((group: ReactGroup) => {
+                    return (
+                      <div
+                        key={group.name}
+                        className="flex flex-col flex-auto p-5 mx-1 my-5 rounded-md md:mx-5 md:p-10 border-2 border-gray-500 bg-coolGray-900"
+                      >
+                        <div>
+                          <div className="flex items-baseline justify-between ">
+                            <h3 className="mb-5 mr-5 text-lg font-medium leading-7 sm:leading-9 sm:truncate">
+                              ⚛ {group.name}
+                            </h3>
+                            <button
+                              onClick={() => setInfoModalOpen(true)}
+                              className="text-xs font-bold text-blue-600 uppercase focus:outline-none"
                             >
-                              📚 Material de estudio
-                            </a>
-                          </li>
-                        </ul>
-                        {group.participants &&
-                        group.participants.length >= 10 ? (
-                          <div className="text-red-500 font-md text-md">
-                            Grupo lleno
+                              + Info
+                            </button>
                           </div>
-                        ) : (
-                          <AddParticipantForm group={group} />
-                        )}
+                          <ul className="mb-6">
+                            <li key={group.topic} className="mb-1 font-medium">
+                              <span className="font-bold">Tema:</span>{' '}
+                              {group.topic}
+                            </li>
+                            <li className="mb-4 font-medium">
+                              <span className="font-bold">
+                                Fecha de inicio:
+                              </span>{' '}
+                              {new Date(group.startDate).toLocaleDateString()}
+                            </li>
+                            <li className="font-medium text-primary">
+                              <a
+                                href={group.studyMaterial}
+                                className="text-informational"
+                              >
+                                📚 Material de estudio
+                              </a>
+                            </li>
+                          </ul>
+                          {group.participants &&
+                          group.participants.length >= 10 ? (
+                            <div className="text-red-500 font-md text-md">
+                              Grupo lleno
+                            </div>
+                          ) : (
+                            <AddParticipantForm group={group} />
+                          )}
+                        </div>
+
+                        <GroupInfoModal
+                          open={infoModalOpen}
+                          onClose={() => setInfoModalOpen(false)}
+                          group={group}
+                        />
                       </div>
+                    );
+                  })}
+                </div>
+              </>
+            )}
 
-                      <GroupInfoModal
-                        open={infoModalOpen}
-                        onClose={() => setInfoModalOpen(false)}
-                        group={group}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-            </>
-          )}
-
-          <div className="flex-1 min-w-0 mb-8">
-            <h1 className="text-2xl font-bold leading-7 text-coolGray-200 title sm:text-2xl sm:leading-9 sm:truncate">
-              Dale vida a un nuevo grupo
-            </h1>
-            <h2 className="leading-7 text-md text-informational sm:leading-9 sm:truncate">
-              <span
-                className="cursor-pointer"
-                onClick={() => setIsModalOpen(true)}
-              >
-                Ver requisitos
-              </span>
-            </h2>
-          </div>
-          <CreateGroupForm />
-        </div>
-
+            <div className="flex-1 min-w-0 mb-8">
+              <h1 className="text-2xl font-bold leading-7 text-coolGray-200 title sm:text-2xl sm:leading-9 sm:truncate">
+                Dale vida a un nuevo grupo
+              </h1>
+              <h2 className="leading-7 text-md text-informational sm:leading-9 sm:truncate">
+                <span
+                  className="cursor-pointer"
+                  onClick={() => setIsModalOpen(true)}
+                >
+                  Ver requisitos
+                </span>
+              </h2>
+            </div>
+            <CreateGroupForm />
+          </di>
+        )}
         <GroupRequirementsModal
           open={isModalOpen}
           onClose={() => setIsModalOpen(false)}
@@ -123,12 +126,14 @@ const ReactGroupPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> =
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
   const data = await getApprovedReactGroups(preview);
   const settings = await getSettings(preview);
+  const isEnabled = process.env.REACTIVISTAS_ENABLED === 'true';
 
   return {
     props: {
       data,
       preview,
       settings,
+      isEnabled,
     },
     revalidate: 1,
   };
