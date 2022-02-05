b11b17fcb26e2c397a44146e9d82265d72504cca
Sebastián Córdoba <sebastian.cordoba@vortex-it.com>
2022-02-05 17:06:19 -0300

    Add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,7 +55,7 @@ module.exports = {
 
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Poppins', 'sans-serif'],
         title: ['Lexend Deca', 'sans-serif'],
       },
       fontSize: {
