9053fcfd94a9e8605e6aad92f29b03a11dad5677
Federico Minaya <elfedeminaya@gmail.com>
2022-04-20 19:11:00 -0300

    refactor: change country to countries

--- src/lib/country.ts
+++ src/lib/country.ts
@@ -14,8 +14,8 @@ export const getUserCountry = async (): Promise<Country> => {
     const res = await fetch(
       `https://restcountries.com/v3.1/name/${ip.country_name.toLowerCase()}`,
     );
-    const country: Country[] = await res.json();
+    const countries: Country[] = await res.json();
 
-    return country[0];
+    return countries[0];
   }
 };
