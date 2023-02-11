2d15b200e4a099bb7527f619b82c73ddc39f92d4
Federico Minaya <elfedeminaya@gmail.com>
2023-02-11 15:38:21 -0800

    feat: add vercel analytics component (#429)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,7 @@ import '@/styles/scrollbar.css';
 import { SettingsProvider } from '@/lib/settings';
 
 import { Lexend_Deca, Rubik } from '@next/font/google';
+import Analytics from '@/components/Analytics';
 
 export const lexend = Lexend_Deca({
   subsets: ['latin'],
@@ -62,6 +63,9 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     <Provider session={pageProps.session}>
       <SettingsProvider settings={pageProps.settings}>
         <Component {...pageProps} />
+
+        <Analytics />
+
         <CookieConsent
           disableStyles
           containerClasses="fixed mb-4 left-0 right-0 bottom-4 flex text-primary justify-between px-4 py-2 items-center bg-zinc-700 z-20 container mx-auto rounded-md"
