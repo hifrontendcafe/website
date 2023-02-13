4b79b75cf2530b78ea171027ff9dc305631fc8f1
Federico Minaya <elfedeminaya@gmail.com>
2023-02-12 18:38:02 -0800

    feat: migrate cmyk to app dir (#434)
    * feat: migrate cmyk to app dir
    
    * fix: move editions to client component

---
+++ src/app/cmyk/page.tsx
@@ -0,0 +1,28 @@
+import { use } from 'react';
+import SectionHero from '@/components/SectionHero';
+import { getPageByName, getAllCMYKProjects } from '@/lib/api.server';
+import type { AppPage } from '@/lib/types';
+import { getPageMetadata } from '@/lib/seo';
+import CMYKEditions from '@/components/CMYKEditions';
+
+export const revalidate = 60;
+
+export const generateMetadata = () => getPageMetadata('CMYK');
+
+const CMYKPage: AppPage = () => {
+  const page = use(getPageByName('CMYK'));
+  const projects = use(getAllCMYKProjects());
+
+  return (
+    <>
+      <SectionHero
+        title={page.title}
+        paragraph={page.description}
+        cta={page.doc}
+      />
+      <CMYKEditions projects={projects} />
+    </>
+  );
+};
+
+export default CMYKPage;
