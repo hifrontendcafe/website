6545e2570f75f6b18190a6e4ed8419df9781f165
Federico Minaya <elfedeminaya@gmail.com>
2023-02-24 02:03:13 -0300

    feat: priority first ten images of team

--- src/app/equipo/page.tsx
+++ src/app/equipo/page.tsx
@@ -18,8 +18,8 @@ export default async function TeamPage() {
       <SectionHero title={page.title} />
       <div className="mb-20">
         <div className="grid grid-cols-1 gap-8 px-6 py-5 text-secondary lg:pt-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 place-content-stretch">
-          {profiles?.map((profile) => (
-            <StaffCard profile={profile} key={profile._id} />
+          {profiles?.map((profile, i) => (
+            <StaffCard index={i} profile={profile} key={profile._id} />
           ))}
         </div>
       </div>
--- src/components/StaffCard/index.tsx
+++ src/components/StaffCard/index.tsx
@@ -5,9 +5,10 @@ import Link from 'next/link';
 
 interface ProfileProp {
   profile: Person;
+  index: number;
 }
 
-const ProfileImage = ({ profile }: ProfileProp) => {
+const ProfileImage = ({ profile, index }: ProfileProp) => {
   return (
     <Image
       className="object-cover mx-auto rounded-full h-36 w-36 md:h-32 md:w-32"
@@ -16,12 +17,13 @@ const ProfileImage = ({ profile }: ProfileProp) => {
       width={128}
       height={128}
       placeholder="blur"
+      priority={index < 10}
       blurDataURL={imageBuilder.image(profile.photo).height(50).url()}
     />
   );
 };
 
-const ProfileText = ({ profile }: ProfileProp) => {
+const ProfileText = ({ profile }: Omit<ProfileProp, 'index'>) => {
   return (
     <h2 className="my-2 text-3xl font-semibold md:text-sm">
       {profile.firstName} {profile.lastName}
@@ -29,17 +31,17 @@ const ProfileText = ({ profile }: ProfileProp) => {
   );
 };
 
-const StaffCard: React.FC<ProfileProp> = ({ profile }) => {
+const StaffCard: React.FC<ProfileProp> = ({ profile, index }) => {
   return (
     <div className="p-2 text-center transition duration-500 ease-in-out scale-100 hover:scale-110">
       {profile.linkedin ? (
         <Link href={profile.linkedin ?? ''} target="_blank" rel="noreferrer">
-          {profile.photo && <ProfileImage profile={profile} />}
+          {profile.photo && <ProfileImage index={index} profile={profile} />}
           <ProfileText profile={profile} />
         </Link>
       ) : (
         <>
-          {profile.photo && <ProfileImage profile={profile} />}
+          {profile.photo && <ProfileImage index={index} profile={profile} />}
           <ProfileText profile={profile} />
         </>
       )}
