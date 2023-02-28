ca07193d2ea6be0dd995818948f47dc591196061
Federico Minaya <elfedeminaya@gmail.com>
2023-02-28 00:18:29 -0300

    fix: removed runtime from cmyk page

--- src/app/cmyk/page.tsx
+++ src/app/cmyk/page.tsx
@@ -12,8 +12,6 @@ export const dynamic = 'auto';
 
 export const fetchCache = 'force-cache';
 
-export const runtime = 'edge';
-
 const CMYKPage: AppPage = async ({ searchParams }) => {
   const page = await getPageByName({
     name: 'CMYK',
