2f8af6c188b9ca2232e4a15e4d6e486fbdd5adbe
Anthony Mackensen <tonymckes@gmail.com>
2023-03-24 13:24:47 -0400

    feat: upgrade storybook to v7rc

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  args: {
+    label: 'Button',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/7.0/react/api/argtypes
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    primary: true,
+  },
+};
+
+export const Secondary: Story = {};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
 import { Header } from './Header';
-import './page.css';
+import './Page.css';
 
 type User = {
   name: string;
 };
 
-export const Page: React.VFC = () => {
+export const Page: React.FC = () => {
   const [user, setUser] = React.useState<User>();
 
   return (
@@ -40,7 +39,7 @@ export const Page: React.VFC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the &lqduo;args&rqduo; of child component stories
+            such data from the "args" of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
