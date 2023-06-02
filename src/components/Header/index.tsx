4fd2a834935130590ca2d69af480cec4be28127a
Anthony Mackensen <tonymckes@gmail.com>
2023-06-01 23:37:38 -0400

    refactor: navbar styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { imageBuilder } from '@/lib/sanity';
 import Navbar from '@/components/Navbar';
-import { useZeroScrollY } from './useScroll';
-import { useMenuToggle } from './useMenuToggle';
+import { imageBuilder } from '@/lib/sanity';
 import { Image, NavItemData } from '@/lib/types';
+import { useMenuToggle } from './useMenuToggle';
+import { useZeroScrollY } from './useScroll';
 
 type HeaderProps = {
   preview?: boolean;
@@ -22,12 +22,14 @@ const Header: React.FC<HeaderProps> = ({ preview, logo, navItems }) => {
 
   return (
     <header
-      className={`w-full flex flex-col fixed top-0 z-50 transition duration-300 ${
+      className={`fixed top-0 z-50 flex w-full flex-col border-b border-[#434345]/75 transition duration-300 ${
         preview ? 'pt-10' : ''
       } ${
         isAtTop && isClosed
-          ? ''
-          : 'items-center bg-zinc-900 shadow-[0px_1px_0px_#434345]'
+          ? 'border-b-transparent'
+          : `bg-zinc-900 ${
+              isAtTop ? 'lg:border-b-transparent lg:bg-transparent ' : ''
+            }`
       }`}
     >
       <Navbar
