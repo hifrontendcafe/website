7186e3f7fb5cebc2a95bcd63d8e200b8ab79ea0c
Anthony Mackensen <tonymckes@gmail.com>
2023-06-05 16:37:40 -0400

    refactor: add mentor and event types

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -125,6 +125,7 @@ export const mentorsTopicsQuery = groq`
 
 export const mentorsQuery = groq`
   *[_type == "mentor" && status in ["ACTIVE", "NOT_AVAILABLE"]] | order(date desc) {
+    'id': persona-> discordID.current,
     name,
     description,
     'photo': {
