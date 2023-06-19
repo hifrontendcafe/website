fb5d04fbe1ad2be8db1f7fd4ba6585e7ba669df0
Anthony Mackensen <tonymckes@gmail.com>
2023-06-19 10:19:44 -0400

    feat: add route layout

--- src/app/cmyk/page.tsx
+++ src/app/cmyk/page.tsx
@@ -1,7 +1,5 @@
 import CMYKEditions from '@/components/CMYKEditions';
 import CMYKEditionsSkeleton from '@/components/CMYKEditions/Skeleton';
-import SectionHero from '@/components/SectionHero';
-import { getPageByName } from '@/lib/api.server';
 import { getPageMetadata } from '@/lib/seo';
 import type { AppPage } from '@/lib/types';
 import { Suspense } from 'react';
@@ -11,26 +9,11 @@ export const generateMetadata = () => getPageMetadata('CMYK');
 export const dynamic = 'auto';
 
 const CMYKPage: AppPage = async ({ searchParams }) => {
-  const page = await getPageByName({
-    name: 'CMYK',
-    next: {
-      revalidate: 60,
-    },
-  });
-
   return (
-    <>
-      <SectionHero
-        title={page.title}
-        paragraph={page.description}
-        cta={page.doc}
-      />
-
-      <Suspense fallback={<CMYKEditionsSkeleton />}>
-        {/* @ts-expect-error Server Component */}
-        <CMYKEditions edition={searchParams.edition} />
-      </Suspense>
-    </>
+    <Suspense fallback={<CMYKEditionsSkeleton />}>
+      {/* @ts-expect-error Server Component */}
+      <CMYKEditions edition={searchParams.edition} />
+    </Suspense>
   );
 };
 
