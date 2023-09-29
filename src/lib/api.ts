d007853d49f07b37b446b5b7f0e0221acb87c05f
Santiago Santana <santiago.santana@aerolab.co>
2023-09-28 22:51:56 -0300

    refactor(src/lib/api): remove unused functions

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,41 +1,10 @@
 import client, { postClient, previewClient } from './sanity';
-import {
-  CMYK,
-  CMYKParticipant,
-  Doc,
-  Event,
-  FeaturedCards,
-  Mentor,
-  Person,
-  Profile,
-  Role,
-  SanityEvent,
-  Seniority,
-  Settings,
-  Technology,
-  Topic,
-} from './types';
+import { CMYKParticipant, Event, Person, Profile, SanityEvent } from './types';
 
 import {
-  cmykQuery,
-  docQuery,
-  docsQuery,
-  eventsQuery,
-  eventsQueryByType,
-  featuredCardsQuery,
-  mentorsQuery,
-  mentorsTopicsQuery,
-  pageQueryByName,
   pagesPathsQuery,
   personQuery,
   personQueryByDiscordID,
-  profileQuery,
-  profilesQuery,
-  rolesQuery,
-  senioritiesQuery,
-  settingsQuery,
-  staffQuery,
-  technologiesQuery,
 } from './queries';
 
 import { pageByPathQuery } from './queries';
@@ -77,21 +46,6 @@ export async function createEvent(
     });
 }
 
-export async function getAllEvents(preview = false): Promise<Event[]> {
-  return await getClient(preview).fetch(eventsQuery);
-}
-
-export async function getSettings(preview = false): Promise<Settings> {
-  return await getClient(preview).fetch(settingsQuery);
-}
-
-export async function getEventsByCategory(
-  preview = false,
-  categoryFilter: string,
-): Promise<Event> {
-  return await getClient(preview).fetch(eventsQueryByType, { categoryFilter });
-}
-
 export async function getAllAPIEvents(
   preview = false,
 ): Promise<Omit<Event, 'description'>> {
@@ -102,29 +56,6 @@ export async function getAllAPIEvents(
   );
 }
 
-export async function getAllMentors(preview = false): Promise<Mentor[]> {
-  return await getClient(preview).fetch(mentorsQuery);
-}
-
-export async function getAllDocs(preview = false): Promise<Doc[]> {
-  return await getClient(preview).fetch(docsQuery);
-}
-
-export async function getDocBySlug(
-  slug: string,
-  preview = false,
-): Promise<Doc> {
-  return await getClient(preview).fetch(docQuery, { slug });
-}
-
-export async function getMentoringTopics(preview = false): Promise<Topic[]> {
-  return await getClient(preview).fetch(mentorsTopicsQuery);
-}
-
-export async function getAllCMYKProjects(preview = false): Promise<CMYK[]> {
-  return await getClient(preview).fetch(cmykQuery);
-}
-
 export async function createCMYKParticipant(
   data: CMYKParticipant,
 ): Promise<CMYKParticipant> {
@@ -132,22 +63,6 @@ export async function createCMYKParticipant(
     ...data,
     _type: 'cmykParticipant',
   });
-}
-
-export async function addParticipantToReactGroup(
-  reactGroupId: string,
-  userId: string,
-): Promise<Person> {
-  return await postClient
-    .patch(reactGroupId)
-    .setIfMissing({ participants: [] })
-    .insert('after', 'participants[-1]', [
-      {
-        _key: userId,
-        _ref: userId,
-      },
-    ])
-    .commit();
 }
 
 export async function createPerson(data: any): Promise<Person> {
@@ -175,54 +90,11 @@ export async function getPersonByDiscordID(
   return result.length > 0 && result[0];
 }
 
-export async function getFecTeam(preview = false): Promise<Person> {
-  const result = await getClient(preview).fetch(staffQuery);
-  return result.length > 0 && result;
-}
-
-export async function getAllFeaturedCards(
-  preview = false,
-): Promise<FeaturedCards[]> {
-  return await getClient(preview).fetch(featuredCardsQuery);
-}
-
-export async function getAllTechnologies(
-  preview = false,
-): Promise<Technology[]> {
-  return await getClient(preview).fetch(technologiesQuery);
-}
-
-export async function getAllSeniorities(preview = false): Promise<Seniority[]> {
-  return await getClient(preview).fetch(senioritiesQuery);
-}
-
-export async function getAllRoles(preview = false): Promise<Role[]> {
-  return await getClient(preview).fetch(rolesQuery);
-}
-
-export async function getAllProfiles(preview = false): Promise<Profile[]> {
-  return await getClient(preview).fetch(profilesQuery);
-}
-
-export async function getProfile(
-  id: string,
-  preview = false,
-): Promise<Profile> {
-  return await getClient(preview).fetch(profileQuery, { id });
-}
-
 export async function getPerson(
   id: string,
   preview = false,
 ): Promise<Profile['person']> {
   return await getClient(preview).fetch(personQuery, { id });
-}
-
-export async function getPageByName(
-  preview = false,
-  name: string,
-): Promise<Page> {
-  return await getClient(preview).fetch(pageQueryByName, { name });
 }
 
 export async function getPagesPaths(preview = false): Promise<string[]> {
