ad657149429558c231f08bae516d688a1c33d20a
Anthony Mackensen <tonymckes@gmail.com>
2023-10-06 05:39:18 -0400

    refactor: mentorship list and card components

--- src/components/MentorList/index.tsx
+++ src/components/MentorList/index.tsx
@@ -8,7 +8,7 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import MentorCard from '../MentorCard';
 import SimpleModal from '../SimpleModal';
-import { requestWarningsStates, useWarnings } from './useWarnings';
+import { useWarnings } from './useWarnings';
 
 interface MentorListProps {
   mentors: Mentor[];
@@ -16,23 +16,14 @@ interface MentorListProps {
   events: DiscordEvent[];
 }
 
-const MentorList: React.FC<MentorListProps> = ({ mentors, topics, events }) => {
+export default function MentorList({
+  mentors,
+  topics,
+  events,
+}: MentorListProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { data: session, status: sessionStatus } = useSession();
-  const loading = sessionStatus === 'loading';
-  const { status, warnings, mentorships } = useWarnings(session?.user.id);
-  const router = useRouter();
-
-  const queryTopic = (topic: string) => {
-    if (!topic) return router.replace('/mentorias');
-
-    const url = new URL(window.location.href);
-    const params = new URLSearchParams(url.search);
-    params.set('especialidad', topic);
-    url.search = `?${params}`;
-
-    router.replace(url.toString());
-  };
+  const { data: session } = useSession();
+  const { warnings, mentorships } = useWarnings(session?.user.id);
 
   const searchParams = useSearchParams()!;
   const speciality = searchParams.get('especialidad') ?? '';
@@ -41,68 +32,38 @@ const MentorList: React.FC<MentorListProps> = ({ mentors, topics, events }) => {
   );
   const filteredMentors = speciality ? filteredByTopic : mentors;
 
+  const mentorHasEvents = (mentor: Mentor) => {
+    const mentorshipChannels = [
+      '756023543304814664',
+      '756023931433123900',
+      '761337525654126592',
+    ] as const;
+
+    return events?.find(
+      (event) =>
+        event.creator_id === mentor.id &&
+        mentorshipChannels.includes(event.channel_id!),
+    );
+  };
+
   return (
     <section className="mt-20">
       <div className="mb-10 flex flex-col justify-between lg:flex-row lg:items-center">
         <h2 className="mb-4 text-2xl font-medium lg:m-0">
           Solicita una mentoría según especialidad
         </h2>
-        <label
-          aria-label="Buscar"
-          className="relative inline-block w-full md:w-1/2 lg:w-1/3"
-        >
-          <select
-            onChange={(event) => queryTopic(event.target.value)}
-            value={speciality}
-            className="w-full appearance-none rounded border border-zinc-400 bg-zinc-900 px-4 py-2 pr-8  leading-tight hover:border-zinc-500 focus:outline-none focus:ring"
-          >
-            <option value="">Buscar...</option>
-            {topics?.map((topic) => (
-              <option value={topic._id} key={topic._id}>
-                {topic.title}
-              </option>
-            ))}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
-            <svg
-              className="h-4 w-4 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-            </svg>
-          </div>
-        </label>
+        <SpecialitySelector speciality={speciality} topics={topics} />
       </div>
       <ul className="grid gap-12 lg:grid-cols-2">
-        {filteredMentors?.map((mentor, index) => {
-          const discordEvent = events?.find(
-            ({ creator_id, channel_id }) =>
-              creator_id === mentor.id &&
-              [
-                '756023543304814664',
-                '756023931433123900',
-                '761337525654126592',
-              ].includes(channel_id!),
-          );
-
-          return (
-            <MentorCard
-              key={index}
-              mentor={mentor}
-              topics={topics}
-              canBookAMentorship={
-                !!session &&
-                !loading &&
-                status === requestWarningsStates.SUCCESS &&
-                warnings === 0 &&
-                mentorships <= 4
-              }
-              openModal={() => setIsModalOpen(true)}
-              event={discordEvent}
-            />
-          );
-        })}
+        {filteredMentors?.map((mentor) => (
+          <MentorCard
+            key={mentor.id}
+            mentor={mentor}
+            topics={topics}
+            openModal={() => setIsModalOpen(true)}
+            event={mentorHasEvents(mentor)}
+          />
+        ))}
       </ul>
 
       <SimpleModal
@@ -159,6 +120,51 @@ const MentorList: React.FC<MentorListProps> = ({ mentors, topics, events }) => {
       </SimpleModal>
     </section>
   );
-};
+}
 
-export default MentorList;
+function SpecialitySelector({
+  speciality,
+  topics,
+}: Pick<MentorListProps, 'topics'> & { speciality: string }) {
+  const router = useRouter();
+
+  const queryTopic = (topic: string) => {
+    if (!topic) return router.replace('/mentorias');
+
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    params.set('especialidad', topic);
+    url.search = `?${params}`;
+
+    router.replace(url.toString());
+  };
+
+  return (
+    <label
+      aria-label="Buscar"
+      className="relative inline-block w-full md:w-1/2 lg:w-1/3"
+    >
+      <select
+        onChange={(event) => queryTopic(event.target.value)}
+        value={speciality}
+        className="w-full appearance-none rounded border border-zinc-400 bg-zinc-900 px-4 py-2 pr-8  leading-tight hover:border-zinc-500 focus:outline-none focus:ring"
+      >
+        <option value="">Buscar...</option>
+        {topics?.map((topic) => (
+          <option value={topic._id} key={topic._id}>
+            {topic.title}
+          </option>
+        ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+        <svg
+          className="h-4 w-4 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </svg>
+      </div>
+    </label>
+  );
+}
