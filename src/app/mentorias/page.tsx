5d07fce000460f693072f7c7dce6b1f455b8f30a
Anthony Mackensen <tonymckes@gmail.com>
2023-10-06 05:42:49 -0400

    refactor: move PageComponents to mentorship layout

--- src/app/mentorias/page.tsx
+++ src/app/mentorias/page.tsx
@@ -1,11 +1,6 @@
 import MentorList from '@/components/MentorList';
 import MentorListSkeleton from '@/components/MentorList/MentorListSkeleton';
-import { PageComponents } from '@/components/Page/Matcher';
-import {
-  getAllMentors,
-  getMentoringTopics,
-  getPageByName,
-} from '@/lib/api.server';
+import { getAllMentors, getMentoringTopics } from '@/lib/api.server';
 import { getAllDiscordEvents } from '@/lib/discord';
 import { getPageMetadata } from '@/lib/seo';
 import { shuffle } from '@/lib/shuffle';
@@ -14,8 +9,7 @@ import { Suspense } from 'react';
 export const generateMetadata = () => getPageMetadata('Mentorías');
 
 export default async function MentorshipsPage() {
-  const [page, topics, mentors] = await Promise.all([
-    getPageByName({ name: 'Mentorías' }),
+  const [topics, mentors] = await Promise.all([
     getMentoringTopics({ next: { revalidate: 180 } }),
     getAllMentors({ next: { revalidate: 60 } }),
   ]);
@@ -37,8 +31,6 @@ export default async function MentorshipsPage() {
 
   return (
     <>
-      <PageComponents components={page.components} />
-
       <Suspense fallback={<MentorListSkeleton />}>
         <MentorList
           topics={topics}
