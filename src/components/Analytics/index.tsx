16b6f6ad0ffc03af4f853f3e7662826612e1266c
Santiago Santana <santiago.santana@aerolab.co>
2024-01-20 21:49:54 -0300

    feat(analytics): replace hotjar for microsoft clarity

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,21 +1,36 @@
-'use client';
-// import { useEffect } from 'react';
-// import { usePathname } from 'next/navigation';
 import Script from 'next/script';
 
 import CookieConsent from './CookieConsent';
 import VercelAnalytics from './Vercel';
 
+function Clarity() {
+  // if (process.env.NODE_ENV !== 'production') {
+  //   return null;
+  // }
+
+  if (!process.env.CLARITY_PROJECT_ID) {
+    return null;
+  }
+
+  return (
+    <Script
+      id="clarity"
+      dangerouslySetInnerHTML={{
+        __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.CLARITY_PROJECT_ID}");`,
+      }}
+    />
+  );
+}
+
 export default function Analytics() {
   return (
     <>
       <CookieConsent />
-
-      <Script
-        strategy="afterInteractive"
-        src={`https://static.hotjar.com/c/hotjar-${process.env.NEXT_PUBLIC_HOTJAR_HJID}.js?sv=${process.env.NEXT_PUBLIC_HOTJAR_HJSV}`}
-      />
-
+      <Clarity />
       <VercelAnalytics />
     </>
   );
