65e56ffc6ab195f1443cf14b7d4020265b03f922
Santiago Santana <santiago.santana@aerolab.co>
2024-08-06 19:30:13 -0300

    fix(sanity/env): use date-fns for formatting

--- sanity.env.ts
+++ sanity.env.ts
@@ -1,7 +1,9 @@
+import { format } from 'date-fns';
+
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '0mjeop5f';
 
-const YYYY_MM_DD = new Date().toLocaleDateString('fr-CA');
+const YYYY_MM_DD = format(new Date(), 'yyyy-MM-dd');
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || YYYY_MM_DD;
 
-export { dataset, projectId, apiVersion };
+export { apiVersion, dataset, projectId };
